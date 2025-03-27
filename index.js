f4aa0289bd06a5f3a3748c6a34c81778e78a34f9
Aniket-Bhat <69003723+Aniket-Bhat@users.noreply.github.com>
2025-03-27 20:31:06 +0530

    Step 3 and test cases
    Step 2 already covered by inital code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const regex = /^[0-9,\-\s]+$|^$/
+const splitRegex = /[\n,]/
+
 function stringCalc(str) {
 	if (!regex.test(str)) throw Error('String is not properly formatted')
 	return str
-		.split(',')
+		.split(splitRegex)
 		.map(Number)
 		.reduce((a, c) => a + c, 0)
 }
--- index.test.js
+++ index.test.js
@@ -16,3 +16,8 @@ test('should handle error when string contains non-numeric values', () => {
 	expect(() => stringCalc('a,b,c')).toThrow('String is not properly formatted')
 	expect(() => stringCalc('1;2;3')).toThrow('String is not properly formatted')
 })
+
+test('should split on new lines as well as commas', () => {
+	expect(stringCalc('1\n2,3,3')).toBe(9)
+	expect(stringCalc('1\n2\n2\n3')).toBe(8)
+})
